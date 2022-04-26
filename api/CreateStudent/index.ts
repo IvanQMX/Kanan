b91a217a5078bd014cc3805adefc5d4e7c70461c
Iván <iquinterom1600@alumno.ipn.mx>
2022-04-25 19:22:32 -0500

    Login Function finished

--- api/CreateStudent/index.ts
+++ api/CreateStudent/index.ts
@@ -8,34 +8,32 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
   const connected = await connectDB();
   if (!connected) {
     return send(503, "Database Unavailable");
-  } else {
-    const studentID = req.body && req.body.studentID;
-    const name = req.body && req.body.name;
-    const paternalSurname = req.body && req.body.paternalSurname;
-    const maternalSurname = req.body && req.body.maternalSurname;
-    const email = req.body && req.body.email;
-    const telephone = req.body && req.body.telephone;
-    const password = req.body && req.body.password;
-    if (!studentID || !name || !paternalSurname || !maternalSurname || !email || !telephone || !password) {
-      return send(
-        400,
-        "Invalid request. Structure must be:\n{\n\tstudentID,\n\tname,\n\tpaternalSurname,\n\tmaternalSurname,\n\temail,\n\ttelephone,\n\tpassword\n}"
-      );
-    } else {
-      const student = {
-        studentID,
-        name,
-        paternalSurname,
-        maternalSurname,
-        email,
-        telephone,
-        password,
-      };
-      const newStudent = new Student(student);
-      await newStudent.save();
-      return send(200, `Student saved - ${newStudent._id}`);
-    }
   }
+  const studentID = req.body && req.body.studentID;
+  const name = req.body && req.body.name;
+  const paternalSurname = req.body && req.body.paternalSurname;
+  const maternalSurname = req.body && req.body.maternalSurname;
+  const email = req.body && req.body.email;
+  const telephone = req.body && req.body.telephone;
+  const password = req.body && req.body.password;
+  if (!studentID || !name || !paternalSurname || !maternalSurname || !email || !telephone || !password) {
+    return send(
+      400,
+      "Invalid request. Structure must be:\n{\n\tstudentID,\n\tname,\n\tpaternalSurname,\n\tmaternalSurname,\n\temail,\n\ttelephone,\n\tpassword\n}"
+    );
+  }
+  const student = {
+    studentID,
+    name,
+    paternalSurname,
+    maternalSurname,
+    email,
+    telephone,
+    password,
+  };
+  const newStudent = new Student(student);
+  await newStudent.save();
+  return send(201, `Student saved - ${newStudent._id}`);
 };
 
 export default httpTrigger;
--- api/Login/index.ts
+++ api/Login/index.ts
@@ -1,17 +1,18 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { connectDB, send } from "../global-functions";
+import { Response } from "../types";
+import Student from "../models/Student";
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<Response> {
   context.log("Login request.");
+  const connected = await connectDB();
+  if (!connected) {
+    return send(503, "Database Unavailable");
+  }
   const studentID = req.body && req.body.studentID;
   const password = req.body && req.body.password;
-  const responseMessage = studentID
-    ? "Hello, " + studentID + ". This HTTP triggered function executed successfully."
-    : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-  context.res = {
-    status: 300,
-    body: responseMessage,
-  };
+  const test = await Student.findOne({ studentID, password });
+  return send(200, test ? true : false);
 };
 
 export default httpTrigger;
