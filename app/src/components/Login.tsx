be14013246b87745a85f2ab8a09439e0b56312fc
Iván <iquinterom1600@alumno.ipn.mx>
2022-04-26 21:07:16 -0500

    Login Test request

--- app/src/components/Login.tsx
+++ app/src/components/Login.tsx
@@ -73,7 +73,7 @@ export default function Login({ sessionState }: Login) {
             <button
               type="button"
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-md font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              onClick={autenticate}
+              onClick={()=>autenticate()}
               disabled={processingRequest}
             >
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
